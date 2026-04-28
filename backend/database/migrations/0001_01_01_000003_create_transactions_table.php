ccba60906b149d91e0d9541477fc0be0c795219d
marcdenzel <marcdenzelanilas@gmail.com>
2026-04-29 02:18:07 +0800

    feat: Added table for databases

---
+++ backend/database/migrations/0001_01_01_000003_create_transactions_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference_no')->unique();
+            $table->foreignId('user_id')->constrained();
+            $table->decimal('total_amount', 10, 2);
+            $table->decimal('cash_received', 10, 2);
+            $table->decimal('change_amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
